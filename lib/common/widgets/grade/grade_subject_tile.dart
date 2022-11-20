315b8d864c4613e71b34ee1861cc15c6b4440ae3
55nknown <annon@annon.me>
2022-11-20 21:19:31 +0100

    subject grade tile diff

--- lib/common/widgets/grade/grade_subject_tile.dart
+++ lib/common/widgets/grade/grade_subject_tile.dart
@@ -6,12 +6,14 @@ import 'package:filcnaplo_mobile_ui/common/average_display.dart';
 import 'package:flutter/material.dart';
 
 class GradeSubjectTile extends StatelessWidget {
-  const GradeSubjectTile(this.subject, {Key? key, this.average = 0.0, this.groupAverage = 0.0, this.onTap}) : super(key: key);
+  const GradeSubjectTile(this.subject, {Key? key, this.average = 0.0, this.groupAverage = 0.0, this.onTap, this.averageBefore = 0.0})
+      : super(key: key);
 
   final Subject subject;
   final void Function()? onTap;
   final double average;
   final double groupAverage;
+  final double averageBefore;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +23,9 @@ class GradeSubjectTile extends StatelessWidget {
     if (average < 2.0 && average >= 1.0) {
       textColor = AppColors.of(context).red;
     }
+
+    final String changeIcon = average < averageBefore ? "▼" : "▲";
+    final Color changeColor = average < averageBefore ? Colors.redAccent : Colors.lightGreenAccent.shade700;
 
     return Material(
       type: MaterialType.transparency,
@@ -41,9 +46,20 @@ class GradeSubjectTile extends StatelessWidget {
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (groupAverage != 0) AverageDisplay(average: groupAverage, border: true),
+            if (groupAverage != 0 && averageBefore == 0.0) AverageDisplay(average: groupAverage, border: true),
             const SizedBox(width: 6.0),
-            if (average != 0) AverageDisplay(average: average)
+            if (averageBefore != 0.0 && averageBefore != average) ...[
+              AverageDisplay(average: averageBefore),
+              Padding(
+                padding: const EdgeInsets.only(left: 6.0, right: 6.0, bottom: 3.5),
+                child: Text(
+                  changeIcon,
+                  style: TextStyle(
+                    color: changeColor,
+                  ),
+                ),
+              ),
+            ],
           ],
         ),
       ),
