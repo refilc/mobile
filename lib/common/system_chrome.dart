5faf290b649604f35dc8741cb90f24030ba5f31c
55nknown <annon@annon.me>
2022-11-21 09:43:48 +0100

    fix nav covering ui elements

--- lib/common/system_chrome.dart
+++ lib/common/system_chrome.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void setSystemChrome(BuildContext context) {
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Theme.of(context).brightness == Brightness.light ? Brightness.dark : Brightness.light,
