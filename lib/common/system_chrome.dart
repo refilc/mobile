2ca893845bd32317ec94369856376f970eb1e8d5
ezyyeah <ezyyeah@gmail.com>
2022-12-28 01:51:20 +0100

    fix bottom navbar color diff

--- lib/common/system_chrome.dart
+++ lib/common/system_chrome.dart
@@ -8,7 +8,7 @@ void setSystemChrome(BuildContext context) {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Theme.of(context).brightness == Brightness.light ? Brightness.dark : Brightness.light,
-    systemNavigationBarColor: Theme.of(context).backgroundColor,
+    systemNavigationBarColor: Theme.of(context).colorScheme.background,
     systemNavigationBarIconBrightness: Theme.of(context).brightness == Brightness.light ? Brightness.dark : Brightness.light,
     statusBarBrightness: Platform.isIOS ? Theme.of(context).brightness : null,
   ));
