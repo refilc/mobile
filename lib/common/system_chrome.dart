768f4b311ff85ed273909dffbb736f8e0a4ea3e8
ezyyeah <ezyyeah@gmail.com>
2022-12-31 13:32:08 +0100

    bottom navbar color related to colorpicker

--- lib/common/system_chrome.dart
+++ lib/common/system_chrome.dart
@@ -8,7 +8,7 @@ void setSystemChrome(BuildContext context) {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Theme.of(context).brightness == Brightness.light ? Brightness.dark : Brightness.light,
-    systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
+    systemNavigationBarColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
     systemNavigationBarIconBrightness: Theme.of(context).brightness == Brightness.light ? Brightness.dark : Brightness.light,
     statusBarBrightness: Platform.isIOS ? Theme.of(context).brightness : null,
   ));
