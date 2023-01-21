439dbe64f46210eeb2ff2fdd99c51e43291f41f9
55nknown <annon@annon.me>
2023-01-21 14:14:40 +0100

    average display none

--- lib/common/average_display.dart
+++ lib/common/average_display.dart
@@ -1,3 +1,4 @@
+import 'package:filcnaplo/theme/colors/colors.dart';
 import 'package:filcnaplo/ui/widgets/grade/grade_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:i18n_extension/i18n_widget.dart';
@@ -10,7 +11,7 @@ class AverageDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color color = gradeColor(context: context, value: average);
+    Color color = average == 0.0 ? AppColors.of(context).text.withOpacity(.8) : gradeColor(context: context, value: average);
 
     String averageText = average.toStringAsFixed(2);
     if (I18n.of(context).locale.languageCode != "en") averageText = averageText.replaceAll(".", ",");
@@ -19,11 +20,12 @@ class AverageDisplay extends StatelessWidget {
       width: border ? 54.0 : 52.0,
       padding: EdgeInsets.symmetric(horizontal: 8.0 - (border ? 2 : 0), vertical: 6.0 - (border ? 2 : 0)),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(45.0),
-          border: border ? Border.fromBorderSide(BorderSide(color: color.withOpacity(.5), width: 3.0)) : null,
-          color: !border ? color.withOpacity(.25) : null),
+        borderRadius: BorderRadius.circular(45.0),
+        border: border ? Border.fromBorderSide(BorderSide(color: color.withOpacity(.5), width: 3.0)) : null,
+        color: !border ? color.withOpacity(average == 0.0 ? .15 : .25) : null,
+      ),
       child: Text(
-        averageText,
+        average == 0.0 ? "-" : averageText,
         textAlign: TextAlign.center,
         style: TextStyle(color: color, fontWeight: FontWeight.w600),
       ),
